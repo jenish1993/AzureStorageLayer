dd88877cbd8f183a79c5b421a1448a141fc8ffdb
Jenish Zinzuvadiya <jenish.zinzuvadiya@hotmail.com>
2015-10-18 13:57:14 +0530

    Azure table

--- AzureStorageLayer/AzureStorageLayer/CloudStorageManager/AzureTablesStorage.cs
+++ AzureStorageLayer/AzureStorageLayer/CloudStorageManager/AzureTablesStorage.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AzureStorageLayer.StorageManagerExceptions;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +10,95 @@ namespace Common.CloudStorageManager
 {
     partial class AzureStorageManager
     {
+        #region Public code minimizer methods
         /// <summary>
-        /// This method saves the data to azure table.
+        /// creates and save the azure table to azure table storage.
         /// </summary>
         /// <typeparam name="T">Table Type name</typeparam>
         /// <param name="tableName">azure table name</param>
         /// <param name="AzureTable">Azure Table object</param>
-        public void SaveToTable<T>(string tableName, T AzureTable)
+        public void SaveToTable(string tableName, TableEntity AzureTable)
         {
             if (storageAccount == null)
                 throw new NullReferenceException("Storage Account is null please check constructor.");
-            
+            CloudTable table = getTable(tableName);
+            TableOperation insertOperation = TableOperation.Insert(AzureTable);
+            table.Execute(insertOperation);
         }
+        /// <summary>
+        /// Insert or Replaces the azure table to azure table storage.
+        /// </summary>
+        /// <param name="tableName">azure table name</param>
+        /// <param name="AzureTable">table object</param>
+        public void UpdateToTable(string tableName, TableEntity AzureTable)
+        {
+            if (storageAccount == null)
+                throw new NullReferenceException("Storage Account is null please check constructor.");
+            try
+            {
+                CloudTable table = getTable(tableName);
+                TableOperation updateOperation = TableOperation.InsertOrReplace(AzureTable);
+                table.Execute(updateOperation);
+            }
+            catch (Exception ex)
+            {
+                throw new StorageOperationException("Table Updation Excetion.  please see inner exception for more details.", ex);
+            }
+        }
+
+        public void DeleteToTable(string tableName, TableEntity AzureTable)
+        {
+            if (storageAccount == null)
+                throw new NullReferenceException("Storage Account is null please check constructor.");
+            try
+            {
+                CloudTable table = getTable(tableName);
+                TableOperation deleteOperation = TableOperation.Delete(AzureTable);
+                table.Execute(deleteOperation);
+            }
+            catch (Exception ex)
+            {
+                throw new StorageOperationException("Table Deletation Exception. please see inner exception for more details.", ex);
+            }
+        }
+
+        /// <summary>
+        /// Gets all table record based on the table name form the azure table storage.
+        /// </summary>
+        /// <param name="tableName">azure table name</param>
+        /// <param name="filter">filter</param>
+        /// <param name="columns">column to select</param>
+        /// <returns></returns>
+        public IEnumerable<DynamicTableEntity> GetTableData(
+            string tableName,
+            string filter = "",
+            IList<string> columns = null)
+        {
+            CloudTable table = getTable(tableName);
+            TableQuery query = new TableQuery();
+
+            if (filter != string.Empty)
+                query.FilterString = filter;
+            if (columns != null)
+                query.Select(columns);
+
+            return table.ExecuteQuery(query);
+        }
+        #endregion
+
+        #region Azure Table Storage Utility Methods
+        /// <summary>
+        /// returns the azure table based on the table name.
+        /// </summary>
+        /// <param name="tableName">azure table name</param>
+        /// <returns></returns>
+        private CloudTable getTable(string tableName)
+        {
+            CloudTableClient cloudTable = storageAccount.CreateCloudTableClient();
+            CloudTable table = cloudTable.GetTableReference(tableName);
+            table.CreateIfNotExists();
+            return table;
+        }
+        #endregion
     }
 }
--- AzureStorageLayer/AzureStorageLayer/StorageManagerExceptions/StorageConnectionNameNotFoundException.cs
+++ AzureStorageLayer/AzureStorageLayer/StorageManagerExceptions/StorageConnectionNameNotFoundException.cs
@@ -8,17 +8,31 @@ namespace Common.StorageManagerExceptions
 {
     class StorageConnectionNameNotFoundException : Exception
     {
+        /// <summary>
+        /// default constructor.
+        /// </summary>
         public StorageConnectionNameNotFoundException()
             : base("Storage Connection Name Not Found. Please Check Web.config or App.config")
         { }
-        public StorageConnectionNameNotFoundException(string message, Exception internalException)
-                    : base(message, internalException)
+        /// <summary>
+        /// sets the message and innerException to base class Exception.
+        /// </summary>
+        /// <param name="message">error message that you want to set.</param>
+        /// <param name="innerException">innerException object.</param>
+        public StorageConnectionNameNotFoundException(string message, Exception innerException)
+                    : base(message, innerException)
         { }
-
-        public StorageConnectionNameNotFoundException(Exception internalException)
-                  : base("Storage Connection Name Not Found. Please Check Web.config or App.config", internalException)
+        /// <summary>
+        /// sets the default message with innerexception.
+        /// </summary>
+        /// <param name="innerException">innerException object</param>
+        public StorageConnectionNameNotFoundException(Exception innerException)
+                  : base("Storage Connection Name Not Found. Please Check Web.config or App.config", innerException)
         { }
-
+        /// <summary>
+        /// sets the message to base class exception.
+        /// </summary>
+        /// <param name="message">exception message</param>
         public StorageConnectionNameNotFoundException(string message)
             : base(message)
         { }
